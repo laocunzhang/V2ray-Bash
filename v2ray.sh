e1c3e4019f60be72905b25bffcc39ade968d1f41
d1ves <466685963@qq.com>
2017-08-23 22:56:54 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 #Disable China
-wget http://iscn.kirito.moe/run.sh
-. ./run.sh
+#wget http://iscn.kirito.moe/run.sh
+#. ./run.sh
 if [[ $area == cn ]];then
 echo "Unable to install in china"
 exit
